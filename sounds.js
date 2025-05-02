f567388bf96119e3b3aeecce39048758b6dc688d
lazysunda6 <sundaylazy130@gmail.com>
2025-05-02 21:45:56 +0600

    Create sounds.js

---
+++ sounds.js
@@ -0,0 +1,13 @@
+// sound.js
+export const sounds = {
+  achievement: new Audio('sounds/achievement.mp3'),
+  attack: new Audio('sounds/attack.mp3'),
+  levelUp: new Audio('sounds/level-up.mp3')
+};
+
+export function playSound(type) {
+  if (sounds[type]) {
+    sounds[type].currentTime = 0; // Перемотка на начало
+    sounds[type].play().catch(() => console.log(`Ошибка воспроизведения звука: ${type}`));
+  }
+}
